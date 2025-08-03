c022767210e4677abb665a32bc31e5c7603832c7
Kurt Stephens <git@kurtstephens.com>
2025-08-02 20:47:36 -0500

    WASD!

--- pygame/game-3.py
+++ pygame/game-3.py
@@ -46,9 +46,9 @@ while True:
 
     # Handle player movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player_x > 0:
+    if keys[pygame.K_a] and player_x > 0:
         player_x -= player_speed
-    if keys[pygame.K_RIGHT] and player_x < screen_width - player_width:
+    if keys[pygame.K_d] and player_x < screen_width - player_width:
         player_x += player_speed
 
     # Update bullet positions
